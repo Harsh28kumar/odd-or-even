835a4c98caad61f031ca0c9020baefda92928c21
Harsh28kumar <85638039+Harsh28kumar@users.noreply.github.com>
2021-06-15 05:35:46 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,6 @@
+number=input("I'll tell you it's even or odd: ")
+number=int(number)
+if number%2==0:
+    print(" The number " + str(number) + " is even")
+else:
+    print("The number "+ str(number)+ " is odd")
